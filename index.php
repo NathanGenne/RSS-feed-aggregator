38d0618b623e55953cfe2520613234ec0991fb47
logeoSE <49406040+logeoSE@users.noreply.github.com>
2022-11-28 16:23:29 +0100

    router setup

--- index.php
+++ index.php
@@ -1,9 +1,27 @@
 <?php    
-require_once './inc/header.php';
+$url = '';
+if(isset($_GET['url'])) {
+    $url = $_GET['url'];
 
-
-
-
-
+    if($url == '') {
+        require 'view/form_connexion_view.php';
+    }
+    
+    elseif(preg_match('#article-([0-9]+)#', $url, $params)) {
+        $idArticle = $params[1];
+        require 'view/form_creation_view.php';
+    }
+    
+    elseif($url = 'inscription') {
+        $idArticle = 12;
+        require 'view/form_creation_view.php';
+    }
+    
+    else {
+        require 'view/error404.php';
+    }
+} else {
+    require 'view/error500.php';
+}
 
 ?>
