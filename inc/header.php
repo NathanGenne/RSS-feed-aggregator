1821c6bf983de7b4f00a0a102aee49c98bac006d
BriceBOKATOLA <91880016+BriceBOKATOLA@users.noreply.github.com>
2022-11-28 14:31:35 +0100

    ddd
    dd

---
+++ inc/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- PAGE settings -->
+    <link rel="icon">
+    <title>projet</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <!-- CSS dependencies -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- scss -->
+    <link rel="stylesheet" href="css/trombi.css">
+    <!-- Script: Navbar on-top -->
+
+    <script src="js/navbar-ontop.js"></script>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">Home</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar3SupportedContent" aria-controls="navbar3SupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse text-center justify-content-end" id="navbar3SupportedContent">
+                <ul class="navbar-nav">
+
+                    <li class="nav-item mx-2 mb-2 my-md-0">
+                        <a class="nav-link" href="Connexion.php">Connexion</a>
+                    </li>
+                    <li class="nav-item mx-2 mb-2 my-md-0">
+                        <a class="nav-link" href="#team">Deconnexion</a>
+                    </li>
+                </ul>
+                <!-- <a class="btn navbar-btn btn-outline-light" href="#contact">Contact</a> -->
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar end -->
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php    
+require_once './inc/header.php';
+
+
+
+
+
+
+?>
