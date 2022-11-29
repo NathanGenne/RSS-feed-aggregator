a0b154364d1c053b77dc3b0522fa38ab8d5ab0c7
logeoSE <49406040+logeoSE@users.noreply.github.com>
2022-11-29 11:05:15 +0100

    start class

--- controllers/home_page_controller.php
+++ controllers/home_page_controller.php
@@ -1,4 +1,10 @@
 <?php
+
+class Home_page_controller extends Home_page_model {
+    public function __construct(object $topics) {
+        
+    }
+}
 $topics = $_SESSION['topics'];
 foreach($topics as $topic) {
     $topic_content = 'https://www.lemonde.fr/'.$topic.'/rss_full.xml';
