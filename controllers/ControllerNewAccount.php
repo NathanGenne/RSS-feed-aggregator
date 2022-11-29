be05ea64d2c55a2aea48bb5b4ea8c8b0530d4006
logeoSE <49406040+logeoSE@users.noreply.github.com>
2022-11-29 15:40:40 +0100

    big push sa mère

---
+++ controllers/ControllerNewAccount.php
@@ -0,0 +1,49 @@
+<?php
+
+    class ControllerNewAccount {
+
+        public function phase1() {
+            require 'views/viewNewAccount1.php';
+        }
+
+        public function phase2() {
+            require 'views/viewNewAccount2.php';
+        }
+
+        public function index() {
+            require 'views/viewLogin.php';
+        }
+
+        public function verify() {
+
+            require 'models/modelLogin.php';
+
+            if (isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password'])) {
+
+                /* Sécurité supplémentaire */
+                $username = htmlspecialchars($_POST['username']);
+                $pwd      = htmlspecialchars($_POST['password']);
+
+                $model = new modelLogin();
+                $user  = $model->get_user($username, $pwd);
+
+            if ( $user ) {
+
+                $_SESSION['valid'] = true;
+                $_SESSION['user_email'] = $user;
+                $_SESSION['user_pwd'] = $user['user_pwd'];
+                unset($_SESSION['login_error']);
+
+                header('Location: ../home');
+
+            } else {
+                $_SESSION['login_error'] = "Mauvais email ou mot de passe";
+                header('Location: ../login');
+            }
+
+        }
+
+    }
+}
+
+?>
---
+++ models/modelNewAccount.php
@@ -0,0 +1,31 @@
+<?php
+
+require './models/db_connect.php';
+
+    class modelNewAccount extends db_connect {
+        /* Récupértion des centres d'intérêts de l'utilisateur selon son index */
+        function get_topics($id)
+        {
+            $db = $this->connexion;
+        
+            $sql = <<<EOD
+            SELECT
+                topics
+            FROM
+                user
+            WHERE user_id = :input_id
+            EOD;
+        
+            $userStmt = $db->prepare($sql);
+            $userStmt->bindValue(':input_id', $id);
+        
+            $userStmt->execute();
+        
+            $topics = $userStmt->fetch(PDO::FETCH_ASSOC);
+            return $topics;
+        }
+    }
+
+
+
+?>
