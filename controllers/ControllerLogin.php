e828b78dbd763b0ce2d51b031630249e97b670ca
logeoSE <49406040+logeoSE@users.noreply.github.com>
2022-12-01 11:05:17 +0100

    pusk

--- controllers/ControllerLogin.php
+++ controllers/ControllerLogin.php
@@ -22,16 +22,19 @@
                 $user  = $model->get_user($email, $pwd);
 
                 if ( $user && $pwd == $_POST['password'] ) {
-                    var_dump($user);
 
                     $_SESSION['id'] = $user['user_id'];
-                    var_dump($user['user_id']);
-                    if( $model->get_verified($_SESSION['id']) === 1 ) {
+                    
+                    if( $model->get_verified($_SESSION['id']) == 1 ) {
                         $_SESSION['verified'] = 1;
                         $_SESSION['mail'] = $email;
                         $_SESSION['pwd'] = $pwd;
 
                         header('Location: ../home');
+
+                    } else {
+                        $_SESSION['login_error'] = "Vous n'êtes pas un utilisateur vérifié";
+                        header('Location: ../login');
                     }
 
                 } else {
